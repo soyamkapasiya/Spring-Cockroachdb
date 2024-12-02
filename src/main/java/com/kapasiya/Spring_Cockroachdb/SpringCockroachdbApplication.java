6872fd90c07db56aafa78d1b9ef923f19b2933e1
Soyam Kapasiya <soyamp323@gmail.com>
2024-12-02 16:08:25 +0530

    initial commit

---
+++ src/main/java/com/kapasiya/Spring_Cockroachdb/SpringCockroachdbApplication.java
@@ -0,0 +1,13 @@
+package com.kapasiya.Spring_Cockroachdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCockroachdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCockroachdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kapasiya/Spring_Cockroachdb/SpringCockroachdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapasiya.Spring_Cockroachdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCockroachdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
